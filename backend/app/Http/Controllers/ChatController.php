9f3cea21fa161e52bbc551b8f5cd1bba595aeb0b
Pierre Santos <santospierre1305@gmail.com>
2020-08-18 11:22:37 +0200

    Backend - Setting Up Pusher + Send Message Event & Api Controller for chat

---
+++ backend/app/Http/Controllers/ChatController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chat;
+use App\Events\SendMessage;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Chat::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newMessage = Chat::create([
+            'message' => $request->message
+        ]);
+
+        event(new SendMessage($request->message));
+
+        return $newMessage;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chat $chat)
+    {
+        //
+    }
+}
--- backend/database/seeds/ChatSeeder.php
+++ backend/database/seeds/ChatSeeder.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Chat;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ChatSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ChatSeeder::class);
+        factory(Chat::class,10)->create();
     }
 }
