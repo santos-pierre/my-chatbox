dbae7fc5a41bd30f51e6bd221486bc5ad95ba75e
Pierre Santos <santospierre1305@gmail.com>
2020-08-18 10:30:19 +0200

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="flex h-screen justify-center items-center flex-col">
+      <h1 className="text-3xl text-center"><span className="text-green-800 font-bold">Vite.js</span></h1>
+      <h2 className="text-3xl">
+        <span className="text-teal-500 font-semibold">React</span> - <span className="font-extrabold text-indigo-500">Tailwind</span>
+      </h2>
+    </div>
+  )
+}
+
